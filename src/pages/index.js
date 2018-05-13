1fae8ea2cd51173c3ba4f963e0c55e4ddae16abf
Austin Lanari <austin.lanari@gmail.com>
2018-05-12 22:52:29 -0700

    query tipe posts and display links

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,14 +6,36 @@ console.log(test)
 console.log(YourDoc)
 
 console.log(JSON.stringify(SomeFolder))
-const IndexPage = () => (
-  <div>
-    <h1>Hi people</h1>
-    <h2 className="subtitle">{YourDoc.block}</h2>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
-)
+const IndexPage = props => {
+  const posts = props.data.allTipePostsJson.edges.map(e => e.node)
+  return (
+      <div>
+        <h1>Hi people</h1>
+        <h2 className="subtitle">{YourDoc.block}</h2>
+        <p>Welcome to your new Gatsby site.</p>
+      <p>Now go build something great.</p>
+      {posts.map(p => {
+        return(
+          <div>
+            <Link to={p.urlSlug}>{p.title}</Link>
+          </div>
+        )
+      })}
+      </div>
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+  query getIndex {
+    allTipePostsJson {
+      edges {
+        node {
+          title
+          urlSlug
+        }
+      }
+    }
+  }
+`
